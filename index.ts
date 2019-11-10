bd1d08745a5776ffe05d18070706984efa06753a
Victor Hugo <vct.bta@gmail.com>
2019-11-10 18:04:25 -0300

    Remove the cors application

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import http from 'http';
 import express from 'express';
-import cors from 'cors';
 import { Server } from 'colyseus';
 import { monitor } from '@colyseus/monitor';
 
@@ -9,7 +8,6 @@ import GameRoom from './src/rooms/GameRoom';
 const port = Number(process.env.PORT || 2567);
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
 const server = http.createServer(app);
