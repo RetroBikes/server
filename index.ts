5382264058df812b1c8c2e090805904f25d38cfa
Victor Hugo <vct.bta@gmail.com>
2019-11-10 18:22:05 -0300

    Try to remove all cors to test from local

--- index.ts
+++ index.ts
@@ -1,35 +1,29 @@
 import http from 'http';
 import express from 'express';
-import cors from 'cors';
 import { Server } from 'colyseus';
 import { monitor } from '@colyseus/monitor';
 
 import GameRoom from './src/rooms/GameRoom';
 
+const allowCrossDomain = (req: any, res: any, next: any) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
+
 const port = Number(process.env.PORT || 2567);
 const app = express();
 
 
-var allowedOrigins = [
-  'http://localhost:3000',
-  'https://retrobikes-client-react.herokuapp.com',
-];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if (! origin) {
-      return callback(null, true);
-    }
-    if (-1 === allowedOrigins.indexOf(origin)) {
-      const msg = 'The CORS policy for this site does not ' +
-        'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(allowCrossDomain);
 
 app.use(express.json());
 
